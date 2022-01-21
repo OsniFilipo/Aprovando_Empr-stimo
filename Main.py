b0653ac05b52736404b93b10250b96c5b95b4b1e
OsniFilipo <osnifilipo@gmail.com>
2022-01-20 23:52:34 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,5 +2,10 @@ casa = float(input('Valor da casa: R$'))
 salário = float(input('Salárario do comprador: R$'))
 anos : int(input('Quantos anos de financiamento? '))
 prestação = casa / (anos * 12)
+minímo = salário * 30 / 100
 print('Para pagar uma casa de R${:.2f} em {}anos' .format(casa, anos))
 print(' a prestação será de R${:.2f}' .format(prestação))
+if prestação <= minímo:
+    print('Empréstimo pode ser CONCEDIDO!')
+else:
+    print('Empréstimo NEGADO!')
